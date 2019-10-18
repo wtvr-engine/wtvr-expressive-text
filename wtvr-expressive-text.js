7a0611daae19298d5706348705fefbd946d81bf0
John Tiesselune <tiesse@wtvr.rocks>
2019-10-18 16:40:29 +0200

    Added reexport for element

--- wtvr-expressive-text.js
+++ wtvr-expressive-text.js
@@ -1,3 +1,4 @@
 import { WTVRExpressiveText } from './src/WTVRExpressiveText.js';
+export { WTVRExpressiveText } from './src/WTVRExpressiveText.js';
 
 customElements.define("wtvr-expressive-text",WTVRExpressiveText);
